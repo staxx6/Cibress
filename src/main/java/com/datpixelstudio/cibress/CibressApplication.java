bd7e65632680e88b6ee54164ea066e9297dea28d
Simon Oczadly <staxx@hotmail.de>
2019-02-27 20:12:22 +0100

    Spring boot start

---
+++ src/main/java/com/datpixelstudio/cibress/CibressApplication.java
@@ -0,0 +1,13 @@
+package com.datpixelstudio.cibress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datpixelstudio/cibress/CibressApplicationTests.java
@@ -0,0 +1,16 @@
+package com.datpixelstudio.cibress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CibressApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
