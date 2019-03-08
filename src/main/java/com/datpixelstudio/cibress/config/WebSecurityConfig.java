25f18c06f6983c1fe30d68c05f46940d7ee3177e
Simon Oczadly <staxx@hotmail.de>
2019-03-08 22:37:28 +0100

    starting with the frontend

--- src/main/java/com/datpixelstudio/cibress/config/WebSecurityConfig.java
+++ src/main/java/com/datpixelstudio/cibress/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.datpixelstudio.cibress.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // calls security config on every request
@@ -32,8 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                    .antMatchers("/noSecurity").permitAll() // ant build system => ant-style path patterns
-                    .antMatchers("/registration").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/noSecurity").permitAll() // ant build system => ant-style path patterns
+                .antMatchers("/registration").permitAll()
                     .antMatchers(HttpMethod.POST, "/registration").permitAll()
 //                    .antMatchers("/admin").hasRole("ADMIN")
                 .and().authorizeRequests()
