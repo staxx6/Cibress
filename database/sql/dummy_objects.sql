239b3ea326791efeb02892b5b6576bc677a86699
Simon Oczadly <staxx@hotmail.de>
2019-03-23 20:48:05 +0100

    created fragment for month-day; need not data from controller; +addDish btn

--- database/sql/dummy_objects.sql
+++ database/sql/dummy_objects.sql
@@ -21,7 +21,7 @@ INSERT INTO ingredient VALUES (1, "Apfel", "0");
 INSERT INTO ingredient VALUES (2, "Birne", "0");
 INSERT INTO ingredient VALUES (3, "Kirsche", "0");
 
-INSERT INTO anonymous_comment VALUES (1, "Das ist absolut nocht meine lieblings Pizza");
+INSERT INTO anonymous_comment VALUES (1, "Das ist absolut nöcht meine Lieblingspizza");
 
 INSERT INTO dish VALUES (1, "Pizza", 0, 1);
 
